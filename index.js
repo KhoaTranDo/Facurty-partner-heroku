a04a3f63085d6059aac88482620bdf4673f3f6b9
Khoatrando <khoatrando516@gmail.com>
2021-05-17 00:44:21 +0700

    ádas

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
   });
   router(app);
-  if(process.env.MONGODB_CONNECTION_STRING==='production'){
+  if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
